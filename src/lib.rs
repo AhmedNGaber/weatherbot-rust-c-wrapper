955938d5eba77788666903005c10b8bff8cb5572
AhmedNGaber <ahmed.n.gaber@gmail.com>
2024-06-02 12:49:21 +0200

    Add initial interfaces to be called from C code
    Signed-off-by: AhmedNGaber <ahmed.n.gaber@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn set_city_name(c_city_name: *const c_char) {
+    // for NULL-terminated C strings, it's a little bit clumsier
+    let city_name = unsafe { CStr::from_ptr(c_city_name).to_string_lossy().into_owned() };
+
+    println!("city_name = <{:?}>", city_name);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[no_mangle]
+pub extern "C" fn set_city_geometry(lat: f64, lng: f64) {
+    println!("Latitude = <{}> Longtude = {}", lat, lng);
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn is_city_valid() -> i32 {
+    return 100;
+}
+
+#[no_mangle]
+pub extern "C" fn get_city_temperature() -> f32 {
+    return 30.5;
 }
